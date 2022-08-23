70b294c1ccddcf1e4be4c1b76c01f6d0662dbb2d
Brandon Vadillo <bvadillo0@ucol.mx>
2022-08-23 12:14:34 -0500

    Faltaba parentesis en linea del footer

--- includes/views/footer.view.php
+++ includes/views/footer.view.php
@@ -2,6 +2,6 @@
 	</div><!--Fin row p-contenido-->
 </div><!--Fin container principal-->
 <script>
-  $(".home-href a").attr("href","/ubicaciones/";
+  $(".home-href a").attr("href","/ubicaciones/");
 </script>
 <?php echo stream_get_contents(fopen('https://www.ucol.mx/cms/footerapp2.php','rb')); ?>
